b72161833d0016ab2988abbdeecbc00c1bbc0d83
sangeeth-sece <157004393+sangeeth-sece@users.noreply.github.com>
2025-01-24 15:05:01 +0530

    portfolio components

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import GlobalStyle from './styles/GlobalStyle';
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+`;
+
+function App() {
+  return (
+    <Router>
+      <AppContainer>
+        <GlobalStyle />
+        <Navbar />
+        <MainContent>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </MainContent>
+        <Footer />
+      </AppContainer>
+    </Router>
+  );
+}
+
+export default App;
